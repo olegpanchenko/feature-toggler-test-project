ae528100bae20a67e5d1c53ae4aeca7340b24322
Oleg Panchenko <o.panchenko@svitla.com>
2025-05-06 14:08:48 +0300

    Add a Mutex around the cache reads/writes

--- app/services/feature_togglers/cache.rb
+++ app/services/feature_togglers/cache.rb
@@ -5,23 +5,34 @@ module FeatureTogglers
     def initialize
       @global_features = nil
       @client_settings = {}
+      @mutex = Mutex.new
     end
 
     def set_global_features(features)
-      @global_features = features
+      @mutex.synchronize { @global_features = features }
+    end
+
+    def global_features
+      @mutex.synchronize { @global_features }
+    end
+
+    def global_features=(features)
+      set_global_features(features)
     end
 
     def client_settings(feature_name)
-      @client_settings[feature_name]
+      @mutex.synchronize { @client_settings[feature_name] }
     end
 
     def set_client_settings(feature_name, settings)
-      @client_settings[feature_name] = settings
+      @mutex.synchronize { @client_settings[feature_name] = settings }
     end
 
     def clear
-      @global_features = nil
-      @client_settings = {}
+      @mutex.synchronize do
+        @global_features = nil
+        @client_settings = {}
+      end
     end
   end
 end
